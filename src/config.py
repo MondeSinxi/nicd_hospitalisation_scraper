efa25018c2bf9d18042ecbbba747fe3cf25b5e0e
MondeSinxi <monde.sinxi@gmail.com>
2021-12-20 19:49:54 +0200

    ENH: fetch files from NICD and store in local directory

---
+++ src/config.py
@@ -0,0 +1,14 @@
+NICD_FILENAME_TEMPLATES = (
+    "NICD-COVID-19-Daily-Sentinel-Hospital-Surveillance-report-National-{date:%Y%m%d}.pdf",
+    "NICD-COVID-19-Daily-Sentinel-Hospital-Surveillance-Report-National-{date:%Y%m%d}.pdf",
+    "Datcov19_National_Export-{date:%Y%m%d}.pdf",
+    "DATCOV-National-report-{date:%Y%m%d}.pdf",
+    "National_datcov_report_{date:%Y%m%d}.pdf",
+    "NICD_COVID-19_Daily_Sentinel_Hospital_Surveillance_report_National_{date:%Y%m%d}.pdf",
+    "Datcov_National_Export_{date:%d_%b}.pdf",
+    "Datcov_National_Export-{date:%d-%b}.pdf",
+    "DATCOV-{date:%d-%B}.pdf",
+    "Daily-DatcovNICD-COVID-19-Daily-Sentinel-Hospital-Surveillance-report-National-20201104pdf.pdf",
+    "NICD-COVID-19-Daily-Sentinel-Hospital-Surveillance-report-Private-{date:%Y%m%d}.pdf",
+    "NICD-COVID-19-Daily-Sentinel-Hospital-Surveillance-report-PRIVATE-{date:%Y%m%d}.pdf"
+)
