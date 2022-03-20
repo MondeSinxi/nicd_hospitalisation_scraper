d4a7e9eba5beff6a63421b30a09fd9de09541d7f
MondeSinxi <monde.sinxi@gmail.com>
2022-03-20 16:21:47 +0200

    REF: column names and ordering

--- src/database/db.py
+++ src/database/db.py
@@ -13,18 +13,16 @@ def generate_engine(sqlalchemy_connection_details="sqlite:///nicd_data.db"):
         __tablename__ = "hospitalisation"
 
         id = Column(Integer, primary_key=True)
-        province = Column(String)
-        facilities_reporting = Column(Integer)
+        facilities_reporting = Column(String)
         admissions_to_date = Column(Integer)
-        deaths_to_date = Column(Integer)
+        died_to_date = Column(Integer)
         discharged_to_date = Column(Integer)
         currently_admitted = Column(Integer)
-        current_in_icu = Column(Integer)
+        currently_in_icu = Column(Integer)
         currently_ventilated = Column(Integer)
-        date = Column(Date)
-        died_to_date = Column(Integer)
         currently_oxygenated = Column(Integer)
         admissions_in_previous_day = Column(Integer)
+        date = Column(Date)
 
     Base.metadata.create_all(engine)
 
